0fc8f9a00647b9e028e502bc2f1020cc7b400905
surajpai50612 <surajpai50612@gmail.com>
2022-08-03 15:58:02 +0530

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  get '/words/:word' => 'meanings#words'
-  resources :meanings
+  get '/meaning/:word' => 'meanings#words'
 end
--- spec/requests/meanings_spec.rb
+++ spec/requests/meanings_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
+require 'rest-client'
 
 RSpec.describe "Meanings", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /meaning/:word" do
+    it "searched data exist" do
+      url = 'https://api.dictionaryapi.dev/api/v2/entries/en/ant'
+      response = RestClient.get(url)
+      expect(response).to match(a_string_including("application/json"))
+    end
+
+    it "searched data donot exist" do
+      url = 'https://api.dictionaryapi.dev/api/v2/entries/en/suraj'
+      response = RestClient.get(url)
+      expect(response.content_type).to match(a_string_including("application/json"))
+    end
   end
 end
